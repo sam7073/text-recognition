df2c3dd1bc35ed08c78391dfd804c5290a791beb
unknown <sam7073@naver.com>
2020-11-25 08:36:34 +0900

    11/25 maybe final

--- server/server.js
+++ server/server.js
@@ -5,21 +5,19 @@ var bodyParser = require('body-parser');
 var Buffer = require('buffer').Buffer;
 const PythonShell = require('python-shell').PythonShell;
 
+// python options
 const options = {
   mode: 'text',
   pythonPath: 'C:/Users/sam70/anaconda3/envs/capstone/python.exe',
   pythonOptions: ['-u'], // get print results in real-time
 };
-
 const app = express();
 
-app.use(bodyParser.json({limit: 10000000}));
+// express request entity limit expand
+app.use(bodyParser.json({limit: 100000000}));
 app.use(cors());
 
-app.get('/data', (req, res) => {
-  res.send({name: 'HelloRNC'});
-});
-
+// processing user upload
 app.post('/upload', (req, res) => {
   fs.writeFile(
     '../image/1.jpg',
@@ -29,10 +27,12 @@ app.post('/upload', (req, res) => {
     },
   );
   PythonShell.run('text_detection.py', options, (err, results) => {
+    console.log(err);
     res.send(results);
   });
 });
 
+// Running on 3001 port
 app.listen(3001, () => {
   console.log('3001 running');
 });
--- server/text_detection.py
+++ server/text_detection.py
@@ -1,34 +1,15 @@
 import cv2
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
 import pathlib
 
-img_height = 200
-img_width = 200
+# img_height = 200
+# img_width = 500
 
-f = open("./dict.txt","r")
-syllable_dict = []
-while True:
-    line = f.readline()
-    if not line:
-        break
-    syllable_dict.append(line[:-1])
+# new_model = tf.keras.models.load_model('D:/model')
 
-new_model = tf.keras.models.load_model('D:/model')
-
-# add = "./image/1.jpg"
-# path = pathlib.Path(add)
-# image = tf.keras.preprocessing.image.load_img(
-#             path, target_size=(img_height, img_width))
-# img_array = tf.keras.preprocessing.image.img_to_array(image)
-# img_array = tf.expand_dims(img_array, 0)
-
-# predictions = new_model.predict(img_array)
-# score = (predictions[0])
-# print(syllable_dict[np.argmax(score)], np.max(score)*100)
-
-kernel = np.ones((37, 37), np.uint8)
+kernel = np.ones((71, 71), np.uint8)
 
 src = cv2.imread("../image/1.jpg")
 
@@ -51,21 +32,23 @@ for contour in contours:
     print()
     print(x + 756, y + 1330, w, h)
 
-    cropped_char = cropped_img[y - 10:y+h + 10, x - 10:x+w + 10]
-    dst = cv2.resize(cropped_char, dsize=(170, 170), interpolation=cv2.INTER_AREA)
-    dst2 = cv2.copyMakeBorder(dst,15,15,15,15,cv2.BORDER_REPLICATE)
-    save_path = "../image/char" + str(index) + ".jpg"
-    index+=1
-    cv2.imwrite(save_path, dst2)
+    # cropped_char = cropped_img[y - 10:y+h + 10, x - 10:x+w + 10]
+    # dst = cv2.resize(cropped_char, dsize=(170, 170), interpolation=cv2.INTER_AREA)
+    # dst2 = cv2.copyMakeBorder(dst,15,15,15,15,cv2.BORDER_REPLICATE)
+    # save_path = "../image/char" + str(index) + ".jpg"
+    # index+=1
+    # cv2.imwrite(save_path, dst2)
     
-    image = tf.keras.preprocessing.image.load_img(save_path, target_size=(img_height, img_width))
-    img_array = tf.keras.preprocessing.image.img_to_array(image)
-    img_array = tf.expand_dims(img_array, 0)
-    predictions = new_model.predict(img_array)
-    score = (predictions[0])
-    print()
-    print(np.argmax(score), np.max(score)*100)
+    # image = tf.keras.preprocessing.image.load_img(save_path, target_size=(img_height, img_width))
+    # img_array = tf.keras.preprocessing.image.img_to_array(image)
+    # img_array = tf.expand_dims(img_array, 0)
+    # predictions = new_model.predict(img_array)
+    # score = (predictions[0])
+    # print()
+    # print(np.argmax(score), np.max(score)*100)
 
+for contour in contours:
+    [x, y, w, h] = cv2.boundingRect(contour)
     cv2.rectangle(src, (x + 756, y + 1330), (x + 756 + w, y + 1330 + h), (255, 0, 255), 3)
 
 cv2.imwrite("../image/final.jpg", final1)
